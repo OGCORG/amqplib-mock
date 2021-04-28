a53fbc142ce665c67d97a70cc12b3cf2328dd68f
MitchellTsai <mit0301@gmail.com>
2021-04-29 00:00:18 +0800

    rename channel.spec.ts

--- src/connection.spec.ts
+++ src/connection.spec.ts
@@ -1,7 +1,7 @@
 import { Connection } from './connection'
 
 describe(`${__filename}`, () => {
-  it('test channel', async () => {
+  it('test connection', async () => {
     const connection = new Connection()
     const channel = await connection.createChannel()
     console.log(channel)
