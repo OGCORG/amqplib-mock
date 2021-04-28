e8b34a01fe1e760e0796b52fe9d9249e3d9509c8
MitchellTsai <mit0301@gmail.com>
2021-04-28 23:44:04 +0800

    add mocha

---
+++ src/channel.spec.ts
@@ -0,0 +1,9 @@
+import { Connection } from "./connection";
+
+describe(`${__filename}`, () => {
+  it('test channel', async () => {
+    const connection = new Connection()
+    const channel = await connection.createChannel()
+    console.log(channel)
+  })
+})
